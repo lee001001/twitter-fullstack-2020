9c63f9915b6c0f918211a5e4409e6b3f23955645
Sharon-liu-go <innovate72095@gmail.com>
2021-01-05 12:07:18 +0800

    hide JWT

--- config/passport.js
+++ config/passport.js
@@ -53,26 +53,26 @@ passport.deserializeUser((id, cb) => {
   })
 })
 
-const jwt = require('jsonwebtoken')
-const passportJWT = require('passport-jwt')
-const ExtractJwt = passportJWT.ExtractJwt
-const JwtStrategy = passportJWT.Strategy
+// const jwt = require('jsonwebtoken')
+// const passportJWT = require('passport-jwt')
+// const ExtractJwt = passportJWT.ExtractJwt
+// const JwtStrategy = passportJWT.Strategy
 
-const jwtOptions = {}
-jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
-jwtOptions.secretOrKey = process.env.JWT_SECRET
+// const jwtOptions = {}
+// jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
+// jwtOptions.secretOrKey = process.env.JWT_SECRET
 
-const strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
-  User.findByPk(jwt_payload.id, {
-    include: [
-      { model: User, as: 'Followers' },
-      { model: User, as: 'Followings' }
-    ]
-  }).then(user => {
-    if (!user) return next(null, false)
-    return next(null, user)
-  })
-})
-passport.use(strategy)
+// const strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+//   User.findByPk(jwt_payload.id, {
+//     include: [
+//       { model: User, as: 'Followers' },
+//       { model: User, as: 'Followings' }
+//     ]
+//   }).then(user => {
+//     if (!user) return next(null, false)
+//     return next(null, user)
+//   })
+// })
+// passport.use(strategy)
 
 module.exports = passport
